bd1def7747fc2865f17179a18f60a48d2f787d82
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-08-04 20:43:47 +0300

    start

--- src/news_agregator/models.py
+++ src/news_agregator/models.py
@@ -6,23 +6,51 @@ class TimeBasedModel(models.Model):
     class Meta:
         abstract = True
 
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-    )
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
-
-class TelegramUser(TimeBasedModel):
+class Group(TimeBasedModel):
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = "Поток"
+        verbose_name_plural = "Список потоков"
 
-    id = models.AutoField(primary_key=True)
-    user_id = models.BigIntegerField(unique=True, verbose_name="UserID")
-    name = models.CharField(max_length=255, verbose_name="UserName")
-    user_role = models.CharField(max_length=255, verbose_name="Роль")
-    state = models.IntegerField(verbose_name="Работает?", default=1)
-    phone = models.CharField(max_length=12, unique=True)
-    chat_id = models.BigIntegerField(verbose_name="Чат пользователя", default=0)
-    chanel_id = models.BigIntegerField(verbose_name="Канал пользователя", default=0)
+    def __str__(self):
+        return self.name
 
+    name = models.CharField(max_length=255, verbose_name="Название потока")
+    visible = models.BooleanField(default=False, verbose_name="Скрыктый поток")
+
+
+class Exam(TimeBasedModel):
+    class Meta:
+        verbose_name = "Экзамен"
+        verbose_name_plural = "Список Экзаменов"
+
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=255, verbose_name="Предмет")
+
+
+class Event(TimeBasedModel):
+    class Meta:
+        verbose_name = "Событие"
+        verbose_name_plural = "Список событий"
+
+    group = models.ForeignKey(Group, on_delete=models.DO_NOTHING, verbose_name="Поток")
+    exam = models.ForeignKey(Exam, on_delete=models.DO_NOTHING, verbose_name="Экзамен")
+    date_time = models.DateTimeField(auto_now_add=False, null=False)
+    link = models.CharField(max_length=4000, null=True)
+
+# class TelegramUser(TimeBasedModel):
+#     class Meta:
+#         verbose_name = "Пользователь"
+#         verbose_name_plural = "Пользователи"
+#
+#     id = models.AutoField(primary_key=True)
+#     user_id = models.BigIntegerField(unique=True, verbose_name="UserID")
+#     name = models.CharField(max_length=255, verbose_name="UserName")
+#     user_role = models.CharField(max_length=255, verbose_name="Роль")
+#     state = models.IntegerField(verbose_name="Работает?", default=1)
+#     phone = models.CharField(max_length=12, unique=True)
+#     chat_id = models.BigIntegerField(verbose_name="Чат пользователя", default=0)
+#     chanel_id = models.BigIntegerField(verbose_name="Канал пользователя", default=0)
